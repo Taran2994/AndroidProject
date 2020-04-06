530a2521d17da2ed49bec729f51d2f34a61c7be3
Navpreet <navpreet717@gmail.com>
2020-04-06 13:27:45 -0500

    Custom Adapter Layout C#19

---
+++ app/src/main/java/com/example/myapplication/EmployeeAdapter.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class EmployeeAdapter {
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,32 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listview);
+
+
+
+
     }
     public void openn(View view) {
         Intent intent = new Intent(this, RegisterationForm.class);
            startActivity(intent);
 
     }
-    public void print(View view)
-    {
-
-       SingletonClass singlobj= SingletonClass.getInstance();
-        //System.out.println("The size is "+ singlobj.emplist.size());
-      for(int i=0;i<singlobj.emplist.size();i++)
-        {
-           Employee emp= singlobj.emplist.get(i);
-            System.out.println(emp.getName());
-            System.out.println(emp.getEmpType());
-        }
 
 
 
-    }
 }
